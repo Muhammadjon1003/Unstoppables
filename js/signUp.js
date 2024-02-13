680649b183d2f90b36fa79b73f86e13bda0e96cb
Mukhlisa8717 <mukhlisa.saidaminova08@gmail.com>
2024-02-13 23:08:02 +0500

    muammo togirlandi

--- js/signUp.js
+++ js/signUp.js
@@ -6,7 +6,8 @@ let passwordInp = get("#password-input");
 let passwordBtn = get(".password-btn");
 let signUpBtn = get('.signUp-btn');
 let loginBtn = get('.login-btn');
-let errorText = get('.login__error-text')
+let uid = new Date().getTime().toString().slice(0, 5);
+let errorText = get('.login__error-text');
 
 passwordBtn.onclick = function() {
     if (passwordInp.getAttribute('type') === 'password') {
@@ -20,15 +21,25 @@ passwordBtn.onclick = function() {
 
 signUpBtn.addEventListener('click', function() {
     let user = JSON.parse(localStorage.getItem('user'));
-    if (usernameInp.value === user.username && passwordInp.value === user.password) {
+    addUserItemToStorage(uid, emailInp.value, usernameInp.value, passwordInp.value);
+    if (emailInp.value && usernameInp.value && passwordInp.value) {
         location.href = '../html/index.html';
-    } else{
-        errorText.textContent = "Неправильное имя пользователя или пароль";
+    } else {
+        errorText.textContent = "Заполните все поля";
+        if (!emailInp.value) {
+            emailInp.style.border = '1px solid red';
+        }
+        if (!usernameInp.value) {
+            usernameInp.style.border = '1px solid red';
+        }
+        if (!passwordInp.value) {
+            passwordInp.style.border = '1px solid red';
+        }
+        throw new Error('User data is incomplete');
     }
 });
 
 
-
 loginBtn.addEventListener('click', function() {
-    location.href = '../html/signUp.html'
+    location.href = '../html/login.html'
 });
